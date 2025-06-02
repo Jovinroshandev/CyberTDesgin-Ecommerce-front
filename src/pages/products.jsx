7c03e33eb73ff095fe59e86a6d5385105bef78ae
jovinroshanj <jovin.roshan.dev@gmail.com>
2025-06-02 16:23:24 +0530

    add env

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react"
-import BannerImageMobile from "../assets/image/Home_Page_Banner_Mobile.png"
-import BannerImageLaptop from "../assets/image/Home_Page_Banner_Laptop.png"
 import ItemCard from "../components/item-card"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
@@ -9,6 +7,7 @@ const backendAPI = process.env.REACT_APP_BACKEND_URI || "http://localhost:5000";
 
 const ShopCard = () => {
     const [products, setProducts] = useState([]);
+    
     // console.log(products)
     useEffect(() => {
         const fetchCardData = async () => {
@@ -40,7 +39,8 @@ const ShopCard = () => {
     );
 };
 
-export default function Home({ setActiveMenu }) {
+export default function Products({ setActiveMenu }) {
+    const [searchInput,setSearchInput] = useState("")
     useEffect(
         () => {
             setActiveMenu("Products")
@@ -51,6 +51,13 @@ export default function Home({ setActiveMenu }) {
         <div>
             {/* Shop Card */}
             <div className="px-4 py-8 md:px-14 lg:px-20">
+                {/* Search box */}
+                <div className="flex justify-between mx-auto w-[98%] py-1 px-2 rounded-full  border border-pink-500 bg-pink-50">
+                    <input value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} placeholder="Find your perfect match.." className="w-[80%] outline-none bg-transparent mx-2" type="text" />
+                    <button className="rounded-full bg-pink-600 text-white font-medium px-3 py-2">Search</button>
+                </div>
+
+                {/* Content */}
                 <h1 className="text-2xl font-bold text-pink-600 mb-4">Best Deals</h1>
 
                 <div className="grid grid-cols-2  gap-y-6 md:gap-y-16  gap-x-5 w-full md:grid-cols-3 lg:grid-cols-5">
