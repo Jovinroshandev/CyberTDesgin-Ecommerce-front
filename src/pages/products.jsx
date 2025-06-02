b838a9bde6629901ad146ba028bb4501c32e87cf
jovinroshanj <jovin.roshan.dev@gmail.com>
2025-06-02 17:05:47 +0530

    fix alignment in products oage

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -52,7 +52,7 @@ export default function Products({ setActiveMenu }) {
             {/* Shop Card */}
             <div className="px-4 py-8 md:px-14 lg:px-20">
                 {/* Search box */}
-                <div className="flex justify-between mx-auto w-[98%] py-1 px-2 rounded-full  border border-pink-500 bg-pink-50">
+                <div className="flex justify-between items-center mx-auto w-[98%] py-1 px-2 rounded-full  border border-pink-500 bg-pink-50">
                     <input value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} placeholder="Find your perfect match.." className="w-[80%] outline-none bg-transparent mx-2" type="text" />
                     <button className="rounded-full bg-pink-600 text-white font-medium px-3 py-2">Search</button>
                 </div>
@@ -60,7 +60,7 @@ export default function Products({ setActiveMenu }) {
                 {/* Content */}
                 <h1 className="text-2xl font-bold text-pink-600 mb-4">Best Deals</h1>
 
-                <div className="grid grid-cols-2  gap-y-6 md:gap-y-16  gap-x-5 w-full md:grid-cols-3 lg:grid-cols-5">
+                <div className="flex flex-wrap gap-5">
                     <ShopCard />
                 </div>
             </div>
