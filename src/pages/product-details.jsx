96f39ac98c35eca6b77dcf052425da7f49043738
jovinroshanj <jovin.roshan.dev@gmail.com>
2025-06-06 18:51:46 +0530

    add product view page

--- src/pages/product-details.jsx
+++ src/pages/product-details.jsx
@@ -5,10 +5,12 @@ export default function ProductDetails() {
   const { image, name, desc } = location.state || {};
 
   return (
-    <div>
-      <img src={image} alt={name} />
-      <h1>{name}</h1>
-      <p>{desc}</p>
+    <div className="my-16">
+      <div className="flex justify-center">
+        <img className="w-64" src={image} alt={name} />
+      </div>
+      <h1 className="mt-5 font-medium text-2xl text-pink-600 text-center">{name}</h1>
+      <p className="mt-2 text-justify mx-10 md:mx-96">{desc}</p>
     </div>
   );
 }
